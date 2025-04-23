e50f0a6c3e866adca53e7499caa72feb382acae2
Nathan Raventos <rav21003@byui.edu>
2025-04-23 12:31:26 -0600

    Added name to explanation

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle.
+
+        //Get the rradius of the circle from the user
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // Compute the area of the circle
+        double area = Math.PI * radius * radius;
+
+        //Display the area for the user to see. 
+        Console.WriteLine($"Area of the circle: {area}");
+
+
+        int x = 3;
+        string s = "Goodbye";
+        float f = 3.14F;
+        double d = 5.21;
+        Console.WriteLine($"Hello,{s} Sandbox World! {x} {f} {d}");
     }
 }
