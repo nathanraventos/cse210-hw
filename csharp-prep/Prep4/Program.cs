7b4cc22b355b9f544dc335d6d4e2fe78c8b3657f
Nathan Raventos <rav21003@byui.edu>
2025-04-28 16:46:16 -0600

    Base program of prep#5, haven't quite finished yet though

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
+List<int> numbers = new List<int>();
+int userNumber = -1;
 
-class Program
+while (userNumber != 0)
 {
-    static void Main(string[] args)
+    Console.WriteLine("Please enter the number you want on the list: ");
+    userNumber = int.Parse(Console.ReadLine());
+    if (userNumber != 0)
     {
-        
+        numbers.Add(userNumber);
+    }
+
+}
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+Console.WriteLine($"The sum of this list is: {sum}");
+
+double average = (double)sum / numbers.Count;
+Console.WriteLine($"The average of the list is: {average}");
+
+int largestNumber = numbers[0];
+foreach (int number in numbers)
+{
+    if (number > largestNumber)
+    {
+        largestNumber = number;
     }
 }
+Console.WriteLine($"The largest number is : {largestNumber}");
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string userName = Console.ReadLine();
+        Console.WriteLine($"Hello there, {userName}!");
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+       Console.WriteLine("What's your favorite number? ");
+       int favoriteNumber = int.Parse(Console.ReadLine());
+       Console.WriteLine($"Wow! {favoriteNumber} is a good number!");
+       return favoriteNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($" Hey {userName}, the square of your favorite number is {squaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int favoriteNum = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNum);
+
+        DisplayResult(name, squaredNumber);
+
     }
 }
