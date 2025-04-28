2a03f6f4719cea859589903135ce4683e1fc5edf
Nathan Raventos <rav21003@byui.edu>
2025-04-28 10:13:34 -0600

    Finished prep3 and started prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("The magic number has been chosen!");
+       
+       int userGuess = -1;
+       int numberOfGuesses = 0;
+
+       while(userGuess != magicNumber)
+       {
+        Console.Write("What is your guess: ");
+        userGuess = int.Parse(Console.ReadLine());
+
+        if(userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if(userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        numberOfGuesses += 1;
+       }
+       Console.WriteLine("You've done it bro!");
+       Console.WriteLine($"It took you {numberOfGuesses} guesses");
+        
     }
 }
