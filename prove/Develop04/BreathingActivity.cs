dc7fc8e73a8ede9bd762916024a742b48701c6dc
Nathan Raventos <rav21003@byui.edu>
2025-06-14 14:20:36 -0600

    Getting rid of trash comments

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -23,37 +23,3 @@ public class BreathingActivity : Activity
         DisplayEndingMessage();
     }
 }
-
-
-
-
-
-
-
-
-
-
-// public class BreathingActivity : Activity
-// {
-//     public BreathingActivity() : base("Breathing Activity",
-//         "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
-//     { }
-
-//     public void Run()
-//     {
-//         DisplayStartingMessage();
-//         DateTime endTime = DateTime.Now.AddSeconds(_duration);
-
-//         while (DateTime.Now < endTime)
-//         {
-//             Console.Write("Breathe in...");
-//             ShowCountdown(4);
-//             Console.WriteLine();
-//             Console.Write("Breathe out...");
-//             ShowCountdown(6);
-//             Console.WriteLine();
-//         }
-
-//         DisplayEndingMessage();
-//     }
-// }
